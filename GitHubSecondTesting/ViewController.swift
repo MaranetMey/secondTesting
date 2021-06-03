72e6a9c966e9e13d80a72053e305c47ab9fdaa8d
Maranet <maranet907@gmail.com>
2021-06-03 09:52:23 +0700

    Update ViewController

--- GitHubSecondTesting/ViewController.swift
+++ GitHubSecondTesting/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //  This is New Branch
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
